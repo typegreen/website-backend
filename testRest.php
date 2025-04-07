57df0d2e26a8e7ccdbd8183d144eb3d1881a13a9
typegreen <paulhumb2013@gmail.com>
2025-04-08 00:17:39 +0800

    NST 21

--- testRest.php
+++ testRest.php
@@ -3,7 +3,7 @@ header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Methods: GET");
 header("Access-Control-Allow-Headers: Content-Type, Authorization");
 
-$apiUrl = "https://oyicdamiuhqlwqckxjpe.supabase.co/rest/v1/ACCOUNTS"; // Change if table name differs
+$apiUrl = "https://oyicdamiuhqlwqckxjpe.supabase.co/rest/v1/accounts"; // Change if table name differs
 $apiKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im95aWNkYW1pdWhxbHdxY2t4anBlIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDM0NDAzMzEsImV4cCI6MjA1OTAxNjMzMX0.tR2W8pggj_yx53UxgHk828u33CqhmzRRzA2xvaRV9g8"; // Replace this with your anon/public API key from Supabase dashboard
 
 $ch = curl_init();
