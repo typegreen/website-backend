6b02fd925ceca593ffc9082d061503da0a938f0e
typegreen <paulhumb2013@gmail.com>
2025-04-14 02:14:04 +0800

    more 2fa

--- verify2FA.php
+++ verify2FA.php
@@ -2,6 +2,13 @@
 header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Methods: POST, OPTIONS");
 header("Access-Control-Allow-Headers: Content-Type, Authorization, apikey");
+header("Content-Type: application/json"); // Always return JSON
+
+if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
+  http_response_code(200);
+  exit;
+}
+
 
 function respond($status, $data) {
     header("Content-Type: application/json");
