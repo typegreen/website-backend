0d63007ccb24c17025735009bb95543770b65d1a
typegreen <paulhumb2013@gmail.com>
2025-05-10 01:52:20 +0800

    add to getLogs.php

--- getLogs.php
+++ getLogs.php
@@ -2,6 +2,7 @@
 header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Methods: POST, GET, OPTIONS, PUT, DELETE");
 header("Access-Control-Allow-Headers: Content-Type, Authorization, apikey");
+header("Content-Type: application/json");
 
 function respond($status, $data) {
     header("Content-Type: application/json");
